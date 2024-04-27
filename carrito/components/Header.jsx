e00f10f9da2563bfe12d208309897b36666900e3
Ruth1318 <124644895+Ruth1318@users.noreply.github.com>
2024-04-27 12:18:22 -0600

    Add files via upload

---
+++ carrito/components/Header.jsx
@@ -0,0 +1,10 @@
+import { Filters } from './Filters.jsx';
+
+export function Header () {
+    return(
+        <header>
+            <h2>Multi Gadgets Tienda Online</h2>
+            <Filters />
+        </header>
+    )
+}
